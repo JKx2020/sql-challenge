1253c591a84577c8a1a21b37224f202ad0261356
Jackie <kamprat2@gmail.com>
2020-02-15 15:55:19 -0500

    employees1986 table

--- EmployeeSQL/Employee1986.sql
+++ EmployeeSQL/Employee1986.sql
@@ -1,8 +1,1 @@
-select e.last_name, e.first_name, d.from_date
-FROM employees as e
-INNER JOIN dept_emp as d
-ON e.emp_no = d.emp_no;
-
-select * from employees1986
-WHERE from_date
-LIKE '1986%';
+select * from employees1986;
